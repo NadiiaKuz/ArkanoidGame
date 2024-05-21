9cbfd1a40805f9fa2c20daae8ab79ad4d7f2d899
Nadiia <nadink1603@gmail.com>
2024-05-21 14:47:20 +0200

    Add project files.

---
+++ FrmArkanoidMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArkanoidGame
+{
+    public partial class FrmArkanoidMain : Form
+    {
+        public FrmArkanoidMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
